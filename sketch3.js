82b99918da987e6bbd0bf40febfe10fbb094a72f
Shovi2023 <124705926+Shovi2023@users.noreply.github.com>
2023-11-14 11:58:39 -0500

    Add files via upload

---
+++ sketch3.js
@@ -0,0 +1,10 @@
+let ele;
+function setup() {
+  ele = createAudio('project2/karenvoice.mp3');
+
+  // here we set the element to autoplay
+  // The element will play as soon
+  // as it is able to do so.
+  ele.autoplay(true);
+}
+
